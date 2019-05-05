820f655728bcfbe8730d754a17b27b168cb3a0ed
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-05 18:38:51 +0700

    setup mongoose model

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
@@ -41,21 +44,41 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return events;
+      return Event
+        .find({})
+        .then(events => {
+          console.log(events);
+          return events;
+        })
+        .catch(err => {
+          console.log(err)
+          throw err;
+        });
     },
-    createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+    createEvent: args => {
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      }
-      events.push(event);
-      return event;
+        date: new Date(args.eventInput.date)
+      });
+      
+      return event
+        .save()
+        .then(res => {
+          console.log(res)
+          return { ...res._doc };
+          // return res;
+        })
+        .catch(err => {
+          console.log(err)
+          throw err;
+        });
     }
   },
   graphiql: true
 }));
 
-app.listen(3000, () => console.log('server running at http://localhost:3000'));
+mongoose.connect('mongodb://localhost:27017/nodejs_graphql_academind', {useNewUrlParser: true})
+  .then(() => app.listen(3000, () => console.log('server running at http://localhost:3000')))
+  .catch(error => console.log('error when trying to connect to mongodb server: ' + err));
