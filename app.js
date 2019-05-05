7ab39416701bdecde61f0ce471cf4025bd3f156d
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-05 16:09:05 +0700

    schema and resolvers

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Fishing', 'Cooking', 'All night coding'];
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}));
 
 app.listen(3000, () => console.log('server running at http://localhost:3000'));
