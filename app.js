10e1ec5bbf23ffdb1ffb9022bf94e5acf240a92e
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-19 16:35:13 +0700

    login and register from the frontend

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -19,5 +30,5 @@ app.use('/graphql', graphqlHttp({
 }));
 
 mongoose.connect('mongodb://localhost:27017/nodejs_graphql_academind', {useNewUrlParser: true})
-  .then(() => app.listen(3000, () => console.log('server running at http://localhost:3000')))
+  .then(() => app.listen(5000, () => console.log('server running at http://localhost:5000')))
   .catch(error => console.log('error when trying to connect to mongodb server: ' + err));
