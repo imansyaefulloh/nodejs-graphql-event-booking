35617fb4d6d070ac2f8b5281542766c71fa0d5ce
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-20 16:59:48 +0700

    add modal

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -5,7 +5,7 @@ import AuthContext from '../../context/auth-context';
 
 const mainNavigation = props => (
   <AuthContext.Consumer>
-    {(context) => {
+    {context => {
       return (
         <header className="main-navigation">
           <div className="main-navigation__logo">
@@ -13,10 +13,20 @@ const mainNavigation = props => (
           </div>  
           <nav className="main-navigation__items">
             <ul>
-              {!context.token && 
-              (<li><NavLink to="/auth">Authenticate</NavLink></li>)}
+              {!context.token && (
+                <li><NavLink to="/auth">Authenticate</NavLink></li>
+              )}
               <li><NavLink to="/events">Events</NavLink></li>
-              {context.token && (<li><NavLink to="/bookings">Bookings</NavLink></li>)}
+              {context.token && (
+                <React.Fragment>
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li>
+                <li>
+                  <button onClick={context.logout}>Logout</button>
+                </li>
+                </React.Fragment>
+              )}
             </ul>
           </nav>
         </header>
