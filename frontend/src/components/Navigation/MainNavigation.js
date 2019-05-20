5ddfaf1100b5bf736ae9dabf26dd7070af6c3e39
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-20 15:55:30 +0700

    using token for redirecting

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './MainNavigation.css';
+import AuthContext from '../../context/auth-context';
 
 const mainNavigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>Easy Events</h1>
-    </div>  
-    <nav className="main-navigation__items">
-      <ul>
-        <li><NavLink to="/auth">Authenticate</NavLink></li>
-        <li><NavLink to="/events">Events</NavLink></li>
-        <li><NavLink to="/bookings">Bookings</NavLink></li>
-      </ul>
-    </nav>
-  </header>
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>Easy Events</h1>
+          </div>  
+          <nav className="main-navigation__items">
+            <ul>
+              {!context.token && 
+              (<li><NavLink to="/auth">Authenticate</NavLink></li>)}
+              <li><NavLink to="/events">Events</NavLink></li>
+              {context.token && (<li><NavLink to="/bookings">Bookings</NavLink></li>)}
+            </ul>
+          </nav>
+        </header>
+      )
+    }}
+  </AuthContext.Consumer>
 );
 
 export default mainNavigation;
