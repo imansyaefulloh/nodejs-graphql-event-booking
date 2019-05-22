dd1f30afd842f8604f2846c4acb5f15d2538f436
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-22 16:09:11 +0700

    fix bugs

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -40,6 +40,11 @@ const events = async eventIds => {
         $in: eventIds
       }
     })
+    
+    events.sort((a, b) => {
+      return (eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString()));
+    });
+
     return events.map(event => {
       return transformEvent(event);
     });
